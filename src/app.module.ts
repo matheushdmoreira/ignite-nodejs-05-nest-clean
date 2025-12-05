7d87a9321c0ac24bc04087f261df9d93527043cc
Matheus Moreira <matheustjp111@gmail.com>
2025-12-05 12:05:12 -0300

    feat: auth module and authenticate controller with jwt sign in

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 import { PrismaService } from './prisma/prisma.service'
@@ -11,8 +13,9 @@ import { PrismaService } from './prisma/prisma.service'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
