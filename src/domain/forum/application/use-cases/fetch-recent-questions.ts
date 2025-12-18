f0e32648c0b10c397c3a26a63f7a754c72865a17
Matheus Moreira <matheustjp111@gmail.com>
2025-12-18 11:06:48 -0300

    feat: communication between layers of fetch recent questions

--- src/domain/forum/application/use-cases/fetch-recent-questions.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common'
+
 import { type Either, right } from '@/core/either'
 
 import { Question } from '../../enterprise/entities/question'
-import type { QuestionsRepository } from '../repositories/questions-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface FetchRecentQuestionsUseCaseRequest {
   page: number
@@ -14,6 +16,7 @@ type FetchRecentQuestionsUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class FetchRecentQuestionsUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
