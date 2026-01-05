4fcecf8e5bd2c69a53561d1e133a700bb8e02b29
Matheus Moreira <matheustjp111@gmail.com>
2026-01-05 08:32:15 -0300

    feat: mappers of prisma

---
+++ src/infra/database/prisma/mappers/prisma-answer-attachment-mapper.ts
@@ -0,0 +1,20 @@
+import type { AttachmentModel as PrismaAttachment } from 'generated/prisma/models'
+
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AnswerAttachment } from '@/domain/forum/enterprise/entities/answer-attachment'
+
+export class PrismaAnswerAttachmentMapper {
+  static toDomain(raw: PrismaAttachment): AnswerAttachment {
+    if (!raw.answerId) {
+      throw new Error('Invalid attachment type.')
+    }
+
+    return AnswerAttachment.create(
+      {
+        attachmentId: new UniqueEntityId(raw.id),
+        answerId: new UniqueEntityId(raw.answerId),
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+}
