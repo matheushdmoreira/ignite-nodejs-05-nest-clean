1deeb12ef4df83c1d1e071b90ba46a1632067227
Matheus Moreira <matheustjp111@gmail.com>
2026-01-05 09:55:25 -0300

    feat: edit question controller and refactor on repositories

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,3 +1,5 @@
+import { Injectable } from '@nestjs/common'
+
 import { type Either, left, right } from '@/core/either'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
@@ -6,8 +8,8 @@ import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-e
 import type { Question } from '../../enterprise/entities/question'
 import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list'
-import type { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository'
-import type { QuestionsRepository } from '../repositories/questions-repository'
+import { QuestionAttachmentsRepository } from '../repositories/question-attachments-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface EditQuestionUseCaseRequest {
   authorId: string
@@ -24,6 +26,7 @@ type EditQuestionUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class EditQuestionUseCase {
   constructor(
     private questionsRepository: QuestionsRepository,
