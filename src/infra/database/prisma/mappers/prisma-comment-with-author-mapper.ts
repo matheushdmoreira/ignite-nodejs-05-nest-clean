4ee15f0fbfbffc47ae3e4ffd740bdbbc6ad4d224
Matheus Moreira <matheustjp111@gmail.com>
2026-01-13 11:31:41 -0300

    feat: prisma comment with author

---
+++ src/infra/database/prisma/mappers/prisma-comment-with-author-mapper.ts
@@ -0,0 +1,23 @@
+import type {
+  CommentModel as PrismaComment,
+  UserModel as PrismaUser,
+} from 'generated/prisma/models'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+
+type PrismaCommentWithAuthor = PrismaComment & {
+  author: PrismaUser
+}
+
+export class PrismaCommentWithAuthorMapper {
+  static toDomain(raw: PrismaCommentWithAuthor): CommentWithAuthor {
+    return CommentWithAuthor.create({
+      commentId: new UniqueEntityId(raw.id),
+      authorId: new UniqueEntityId(raw.author.id),
+      author: raw.author.name,
+      content: raw.content,
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    })
+  }
+}
