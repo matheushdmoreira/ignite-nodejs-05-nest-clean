f9e22a0ac75d8ee67d196cd1a03bc81281d1db43
Matheus Moreira <matheustjp111@gmail.com>
2026-01-15 11:18:16 -0300

    feat: test persist on cache

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -3,12 +3,21 @@ import { randomUUID } from 'node:crypto'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { config } from 'dotenv'
 import { PrismaClient } from 'generated/prisma/client'
+import { Redis } from 'ioredis'
 import { DomainEvents } from '@/core/events/domain-events'
+import { envSchema } from '@/infra/env/env'
 
 config({ path: '.env', override: true, quiet: true })
 config({ path: '.env.test', override: true, quiet: true })
 
-if (!process.env.DATABASE_URL || !process.env.DATABASE_SCHEMA) {
+const env = envSchema.parse(process.env)
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
+
+if (!env.DATABASE_URL || !env.DATABASE_SCHEMA) {
   throw new Error(
     'Please provider a DATABASE_URL or DATABASE_SCHEMA environment variable',
   )
@@ -17,7 +26,7 @@ if (!process.env.DATABASE_URL || !process.env.DATABASE_SCHEMA) {
 let prisma: PrismaClient
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  const url = new URL(process.env.DATABASE_URL!)
+  const url = new URL(env.DATABASE_URL!)
 
   url.searchParams.set('schema', schemaId)
 
@@ -38,6 +47,8 @@ beforeAll(async () => {
   })
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   execSync('pnpm prisma migrate deploy')
 })
